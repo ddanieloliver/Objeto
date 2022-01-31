7ff4fc74892bc2908a55f1092c48c0cb82bae5a1
Daniel OLiveira <dnielvitoroliveira456@gmail.com>
2022-01-31 20:08:37 -0300

    Aprendendo conceitos básicos do mundo dos Objetos

---
+++ oo.js
@@ -0,0 +1,29 @@
+// CÓDIGO NÃO EXECUTÁVEL
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento() // foco passou a ser o objeto 
+
+// o objeto funciona como uma "capsula" que comporta os atributos e as ações
+
+//Principios importantes:
+
+/*
+1. Abstração
+2. Encapsulamento -- Muito importante para a fluidez do sistema pois evita a codependencia
+3. Herança (prototype) -- Recebe atributos e comportamentos de um obj "Pai"
+4. Polimorfismo
+*/
+
+//Priorizar a composição ao invés da herança
